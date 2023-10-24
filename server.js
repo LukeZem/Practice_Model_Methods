c027af5b818c83ed9cbde6cce88cf0627bdcf8b1
LukeZem <lucaszemlin@outlook.com>
2023-10-24 11:49:01 -0700

    finished

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(helmet());
 
 // find   - finds everything
 // .find()
- 
+
 // findById
 
 // insertMany
@@ -33,11 +33,46 @@ app.post('/books', async (req, res) => {
     // in the request there should be an array of books objects.
     let books = req.body.books;
 
-    let dbResponse =  await  Book.insertMany(books);
+    let dbResponse = await Book.insertMany(books);
     res.send(dbResponse);
-})
+});
 
 // findOne
+app.get("/books", async (req, res) => {
+    let bookToFind = req.body.books
+    let dbResponse = await Book.findOne(bookToFind)
+    res.send(dbResponse);
+});
+
+//update one book title based on title
+app.put("/books", async (req, res) => {
+    let oldTitle = req.body.books.title;
+    let newTitle = req.body.books.newTitle
+    let dbResponse = await Book.updateOne({ title: oldTitle }, { title: newTitle }, { new: true });
+    res.send(dbResponse)
+});
+
+// create new book
+app.post("/books", async (req, res) => {
+    let newBook = req.body.books
+    let dbResponse = await Book.create(newBook);
+    res.send(dbResponse)
+});
+
+// delete a book by its id
+app.delete("/books", async (req, res) => {
+    let bookId = req.body.books.id
+    let deletedBook = await Book.findByIdAndDelete(bookId);
+    res.send(deletedBook)
+});
+
+// find all books larger than X pages
+app.get("/books/pages", async (req, res) => {
+    let largeBook = req.body.books.pages;
+    let bigBooks = await Book.find({ pages: { $gt: largeBook } }).exec();
+    res.send(bigBooks)
+});
+
 
 // END ROUTES //
 
